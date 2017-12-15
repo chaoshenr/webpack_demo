9e7ae98db5f580bb569f993495d19360ecdf2637
超神 <jinchao233@gmail.com>
2017-12-15 18:25:21 +0800

    hh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from "lodash";
-import './style.css'
+import './style.css';
+import icon from "./image01.png";
+import printMe from "./print.js";
+
 function component() {
 	var element = document.createElement("div");
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.innerHTML = _.join(['Hello', "金超"], ' ');
 	element.classList.add('hello');
+
+	var image = new Image();
+	image.src = icon;
+	element.appendChild(image);
+
+	var button = document.createElement("button");
+	button.onclick = printMe;
+	button.innerHTML = "printMe";
+	element.appendChild(button);
 	return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const config = {
-	entry: './src/index.js',
+	entry: {
+		app: "./src/index.js",
+		print: './src/print.js'
+	},
 	output: {
-	    filename: 'bundle.js',
+	    filename: '[name].bundle.js',
 	    path: path.resolve(__dirname, 'dist')
 	},
+	devServer: {
+		contentBase: './dist'
+	},
+	// 插件
+	plugins: [
+		// 清理生成目录
+		new CleanWebpackPlugin(['dist']),
+		// html自生成依赖
+		new HtmlWebpackPlugin({
+			title: "Output Management"
+		})
+	],
+	// 定位报错问题件（开发环境使用）
+	devtool: 'inline-source-map',
 	module: {
 		rules: [
 			{
@@ -13,6 +31,18 @@ const config = {
 				use: [
 					'style-loader',
 					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test:  /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
 				]
 			}
 		]
